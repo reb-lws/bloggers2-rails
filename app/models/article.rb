ab86e881fe68f75d3021f2fc829d29803a0b830b
enricob <enrico.baculinao@gmail.com>
2016-06-30 16:21:52 +0800

    added paperclip gem

--- app/models/article.rb
+++ app/models/article.rb
@@ -2,11 +2,16 @@ class Article < ActiveRecord::Base
   has_many :comments
   has_many :taggings
   has_many :tags, through: :taggings
+  # paperclip declarations
+  has_attached_file :image
+  validates_attachment_content_type :image, :content_type => ["image/jpg", "image/jpeg", "image/png"]
 
   def tag_list
     tags.join(", ")
   end
 
+
+  # split incoming tags properly and add them to the tags array
   def tag_list=(tags_string)
     tag_names = tags_string.split(",").collect{|s| s.strip.downcase}.uniq
     new_or_found_tags = tag_names.collect{ |name| Tag.find_or_create_by(name: name)}
